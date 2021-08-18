8e6db42f5423f34e9a8f2acc8b3ac06b2c4bdeee
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-08-18 17:10:07 +0530

    1913. Maximum Product Difference Between Two Pairs
    The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
    
        For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
    
    Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
    
    Return the maximum such product difference.
    
     
    
    Example 1:
    
    Input: nums = [5,6,2,7,4]
    Output: 34
    Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
    The product difference is (6 * 7) - (2 * 4) = 34.
    
    Example 2:
    
    Input: nums = [4,2,5,9,7,4,8]
    Output: 64
    Explanation: We can choose indices 3 and 6 for the first pair (9, 8) and indices 1 and 5 for the second pair (2, 4).
    The product difference is (9 * 8) - (2 * 4) = 64.
    
     
    
    Constraints:
    
        4 <= nums.length <= 104
        1 <= nums[i] <= 104

---
+++ maximum_pro_diff.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int maxProductDifference(vector<int>& nums) 
+{
+    sort(nums.begin(), nums.end());
+    reverse(nums.begin(), nums.end());
+    int max = (nums[0] * nums[1]) - (nums[nums.size() - 1] * nums[nums.size() - 2]);
+    return max;
+}
+
+int main()
+{
+    vector<int> nums{5, 6, 2, 7, 4};
+
+    int maximum = maxProductDifference(nums);
+
+    cout << maximum;
+    return 0;
+}
