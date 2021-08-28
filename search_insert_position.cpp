87df23e4f1d78ef56c735390c0507006cfdb86eb
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-08-29 01:19:28 +0530

    35. Search Insert Position
    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    
    You must write an algorithm with O(log n) runtime complexity.
    
     
    
    Example 1:
    
    Input: nums = [1,3,5,6], target = 5
    Output: 2
    
    Example 2:
    
    Input: nums = [1,3,5,6], target = 2
    Output: 1
    
    Example 3:
    
    Input: nums = [1,3,5,6], target = 7
    Output: 4
    
    Example 4:
    
    Input: nums = [1,3,5,6], target = 0
    Output: 0
    
    Example 5:
    
    Input: nums = [1], target = 0
    Output: 0
    
     
    
    Constraints:
    
        1 <= nums.length <= 104
        -104 <= nums[i] <= 104
        nums contains distinct values sorted in ascending order.
        -104 <= target <= 104

---
+++ search_insert_position.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int searchInsert(vector<int> &nums, int target)
+{
+    //1 3 5 6  t -> 5
+    int low{0};
+    int high = (nums.size() - 1);
+    int mid{0};
+
+    while (low <= high)
+    {
+        mid = ((high + low) / 2);
+        if (target == nums[mid])
+            return mid;
+        else if (nums[mid] < target)
+            low = mid + 1;
+        else if (nums[mid] > target)
+            high = mid - 1;
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> v{1, 3, 5, 6};
+    int found = searchInsert(v, 7);
+    cout << found;
+    return 0;
+}
