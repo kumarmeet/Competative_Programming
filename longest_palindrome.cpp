4a141b3bc96dc980f7f01d8de21f28b516376292
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-09-15 13:10:15 +0530

    409. Longest Palindrome
    Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
    
    Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
    
     
    
    Example 1:
    
    Input: s = "abccccdd"
    Output: 7
    Explanation:
    One longest palindrome that can be built is "dccaccd", whose length is 7.
    
    Example 2:
    
    Input: s = "a"
    Output: 1
    
    Example 3:
    
    Input: s = "bb"
    Output: 2
    
     
    
    Constraints:
    
        1 <= s.length <= 2000
        s consists of lowercase and/or uppercase English letters only.

---
+++ longest_palindrome.cpp
@@ -0,0 +1,14 @@
+ int longestPalindrome(string s) {
+       int count[128] {0};
+
+    for (char c: s)
+        count[c]++;
+
+    int ans = 0;
+    for (int v: count) {
+        ans += v / 2 * 2;
+        if (ans % 2 == 0 && v % 2 == 1)
+            ans++;
+    }
+    return ans; 
+    }
