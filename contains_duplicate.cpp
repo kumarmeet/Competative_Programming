e524031edc95e1b6711ad276e6cd30ade8e274cf
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-09-27 00:57:34 +0530

    217. Contains Duplicate

--- contains_duplicate.cpp
+++ contains_duplicate.cpp
@@ -1,17 +1,10 @@
 bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        bool hasDuplicate = false;
+        bool containsDuplicate(vector<int>& nums) {
+      sort(nums.begin(), nums.end());
         
         for(int i = 0; i < nums.size() - 1; i++)
-        {
             if(nums[i] == nums[i + 1])
-            {
-                hasDuplicate = true;
-                break;
-            }
-        }
-        
-        if(hasDuplicate)
-            return true;
+                return true;
         return false;
-    }
+}
+
